3ba745b936530ac6e3915ff199b3ae9911019c12
Martin Nogueroels <martin.nogueroles@gmail.com>
2022-01-13 21:46:01 -0300

    2142

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import Footer from './components/Footer';
-import Nav from './components/Nav'
-
+import Nav from './components/Nav';
+import ItemListContainer from './components/ItemListContainer';
+import './components/styles/main.css'
 function App() {
   return (
     <div className="App">
       <Nav/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My first store
-        </p>
-        <a
-          className="App-link"
-          href="https://es.reactjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ver más
-        </a>
-      </header>
+      <div className='items_container'>
+        <ItemListContainer item="Zapatilla"/>
+        <ItemListContainer item="Remera"/>
+        <ItemListContainer item="Camisa"/>
+        <ItemListContainer item="Pantalon"/>
+      </div>
       <Footer  />
     </div>
   );
