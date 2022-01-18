6aa8c5199a90d590e93aaacdae44d3e264386941
Martin Nogueroels <martin.nogueroles@gmail.com>
2022-01-17 21:00:45 -0300

    17/1 2100

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from './components/Footer';
 import Nav from './components/Nav';
 import ItemListContainer from './components/ItemListContainer';
 import './components/styles/main.css'
+
 function App() {
   return (
     <div className="App">
