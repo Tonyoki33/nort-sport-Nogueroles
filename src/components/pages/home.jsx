c3123bbcbbb2a0bbb932b41d1266a595efeda367
Martin Nogueroels <martin.nogueroles@gmail.com>
2022-10-17 13:14:42 -0300

    last update

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -2,12 +2,10 @@ import ItemListContainer from "../ItemListContainer";
 import "../styles/main.css";
 import "../services/itemsServer";
 
-
 export const Home = () => {
-
   return (
     <div>
-      <ItemListContainer/>
+      <ItemListContainer />
     </div>
   );
 };
